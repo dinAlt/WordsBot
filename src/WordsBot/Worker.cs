9cf700a798fe2eb3c19b4008f536105f3538a333
dinAlt <dinalt2@gmail.com>
2021-07-17 12:54:25 +0300

    Apply database migrations before bot start

--- src/WordsBot/Worker.cs
+++ src/WordsBot/Worker.cs
@@ -4,25 +4,25 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Telegram.Bot;
 using WordsBot.Common;
-using WordsBot.Common.Views;
-using WordsBot.Database.Sqlite;
-using WordsBot.Translators.YandexTranslate;
+using WordsBot.Common.Models;
 
 namespace WordsBot
 {
   public class Worker : BackgroundService
   {
-    private readonly ILogger<Worker> _logger;
-    private readonly IWordsBot _wordsBot;
-    private readonly IHostApplicationLifetime _applicationLifetime;
+    readonly ILogger<Worker> _logger;
+    readonly IWordsBot _wordsBot;
+    readonly IHostApplicationLifetime _applicationLifetime;
+    readonly IServiceProvider _serviceProvider;
 
-    public Worker(ILogger<Worker> logger, IHostApplicationLifetime applicationLifetime, IWordsBot wordsBot) =>
-      (_logger, _applicationLifetime, _wordsBot) = (logger, applicationLifetime, wordsBot);
+    public Worker(ILogger<Worker> logger, IHostApplicationLifetime applicationLifetime,
+      IServiceProvider serviceProvider, IWordsBot wordsBot) =>
+      (_logger, _applicationLifetime, _serviceProvider, _wordsBot) =
+        (logger, applicationLifetime, serviceProvider, wordsBot);
 
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -30,6 +30,11 @@ namespace WordsBot
       _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
       try
       {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+          ((ProgramDbContext)scope
+            .ServiceProvider.GetRequiredService<WordsBotDbContext>()).Database.Migrate();
+        }
         await _wordsBot.Run(stoppingToken);
       }
       catch (Exception ex)
